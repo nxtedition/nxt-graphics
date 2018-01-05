a0f648f06c99ca0110c08f691758eae67064a902
Robert Nagy <ronagy@icloud.com>
2018-01-05 21:44:42 +0100

    more realistic example

--- src/subt/subt.js
+++ src/subt/subt.js
@@ -3,13 +3,15 @@ import { TweenLite } from 'gsap'
 
 CG.register({
   preview () {
+    this.isPreview = true
+
     // as CCG XML
     this.update(CG.xml`
       <templateData>
-        <componentData id="instance1">
+        <componentData id="f0">
           <data id="text" value="Text displayed in a CasparTextField" />
         </componentData>
-        <componentData id="instance2">
+        <componentData id="f1">
           <data id="imagePath" value="d:/caspar/_TEMPLATEMEDIA/myImage.png" />
           <data id="alpha" value="0.6" />
         </componentData>
@@ -21,10 +23,10 @@ CG.register({
 
     // as JS Object
     this.update({
-      instance1: {
+      f0: {
         text: 'Text displayed in a CasparTextField'
       },
-      instance2: {
+      f1: {
         imagePath: 'd:/caspar/_TEMPLATEMEDIA/myImage.png',
         alpha: '0.6'
       },
@@ -34,7 +36,7 @@ CG.register({
     })
 
     this.play()
-    setTimeout(() => this.stop(), 2000)
+    setTimeout(() => this.stop(), 4000)
   },
 
   load () {
@@ -50,7 +52,7 @@ CG.register({
 
   update (data) {
     this.setState({
-      text: data.instance1 && data.instance1.text
+      text: data.f0 && data.f0.text
     })
   },
 
@@ -64,11 +66,57 @@ CG.register({
 
   render () {
     const { opacity, text } = this.state
-    return (
-      <div style={{
+
+    const styles = {
+      outer: {
+        width: '1280px',
+        height: '720px',
+        backgroundColor: this.isPreview && 'grey'
+      },
+      inner: {
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        position: 'absolute',
+        maxWidth: '1093px',
+        height: '74px',
+        top: '540px',
+        left: '99px',
+        color: 'white',
+        lineHeight: 1,
+        // TODO import font
+        fontFamily: 'openSans',
+        textTransform: 'uppercase',
         opacity
-      }}>
-        Hello {text}
+      },
+      line1: {
+        margin: '0px',
+        background: 'rgba(0,0,0,0.4)',
+        fontSize: '31px',
+        fontWeight: '700',
+        paddingTop: '5px',
+        paddingBottom: '4px',
+        paddingLeft: '15px',
+        paddingRight: '15px'
+      },
+      line2: {
+        margin: '0px',
+        background: '#EC2B8C',
+        fontSize: '23px',
+        fontWeight: '600',
+        letterSpacing: '1px',
+        paddingTop: '5px',
+        paddingBottom: '6px',
+        paddingLeft: '15px',
+        paddingRight: '15px'
+      }
+    }
+
+    return (
+      <div style={styles.outer}>
+        <div style={styles.inner}>
+          <p style={styles.line1}>Adam Leah</p>
+          <p style={styles.line2}>{text}</p>
+        </div>
       </div>
     )
   }
