a4219344178dc58cb195a07c71582c7fc0f65839
Martin Grönlund <martingronlund@live.se>
2018-01-18 18:45:48 +0100

    refactor(video): move to own folder again

--- src/stripe2/index.js
+++ src/stripe2/index.js
@@ -5,7 +5,7 @@ import { TweenLite, Power4 } from 'gsap'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import nxtPic from '../images/nxt-logo.jpg'
-import bbb from './video/big-buck-bunny_trailer.webm'
+import bbb from '../video/big-buck-bunny_trailer.webm'
 import Measure from 'react-measure'
 
 class Template extends CG.Template {
