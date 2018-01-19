5aab906a0cb92cd4d8bd1fcebcb86358f78d8349
Martin Grönlund <martingronlund@live.se>
2018-01-19 14:27:38 +0100

    refactor(webpack): no long lines

--- webpack.config.js
+++ webpack.config.js
@@ -101,7 +101,10 @@ function compileTemplate (template) {
       }),
       new FileManagerPlugin({
         onEnd: {
-          copy: config.postBuild.copyTo.map(destination => ({ source: path.join(__dirname, 'templates', template), destination: path.join(destination, template) }))
+          copy: config.postBuild.copyTo.map(destination => ({
+            source: path.join(__dirname, 'templates', template),
+            destination: path.join(destination, template)
+          }))
         }
       })
     ],
